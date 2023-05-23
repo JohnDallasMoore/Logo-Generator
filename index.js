10fb51085790a357dbe656108a18900715fa39d1
Dallas Moore <moore.johndallas@gmail.com>
2023-05-23 13:52:09 -0500

    Added triang, circle and square files and added their shape markup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const questions = require('./lib/questions');
 const generateLogo = require('./lib/generateLogo');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Shapes = require('./lib/shapes');
 
 function askQuestions(){
     inquirer.prompt(questions).then(function(answers){
